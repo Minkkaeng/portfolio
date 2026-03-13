2cdec97e7da692b6c479e5ab97440f21b0bce514
Minkkaeng <choi90326@gmail.com>
2026-03-13 13:50:49 +0900

    header add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Moon, Sun, Monitor } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 
 type HeaderProps = {
